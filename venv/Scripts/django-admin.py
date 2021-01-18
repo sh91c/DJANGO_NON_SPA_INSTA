1bdbf7e21d14d8e6cd48eeaa1f643e411e5866f8
Choi Sang Hoon <sh91cxp@gmail.com>
2021-01-18 10:31:09 +0900

    clear git cache

--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\choi\documents\github\django_non_spa_insta\venv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
